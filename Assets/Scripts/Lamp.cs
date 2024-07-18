41f5b6e384d4f0875057d4104b09be61f8f8f862
railey13 <nikosrailey@gmail.com>
2024-07-18 23:27:13 +0800

    added light up when near a soul

--- Assets/Scripts/Lamp.cs
+++ Assets/Scripts/Lamp.cs
@@ -9,25 +9,47 @@ public class Lamp : MonoBehaviour {
 
     private Light light;
     private List<GameObject> souls;
+    private GameObject soul = null;
+
+
+    private void CalculateDistance() {
+        float distance = Vector3.Distance(this.transform.position, this.soul.transform.position);
+        Debug.Log(distance);
+
+        distance = Mathf.Clamp(distance, 0 , 5.1f);
+
+        float intensity = Mathf.Lerp(1.0f, 0.1f, distance / 5.1f);
+
+        this.light.intensity = intensity;
+    }
 
     private void Start() {
-        this.light = GetComponent<Light>();
+        this.light = this.CandleLight.GetComponent<Light>();
         this.souls = GhostManager.Instance.Possessed;
     }
 
     private void Update() {
-
+        if(this.soul != null) {
+            this.CalculateDistance();
+        }
+        else {
+            this.light.intensity = 0.0f;
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
         Animator animator = other.GetComponentInParent<Animator>();
         if (animator) {
-            Debug.Log("I HATE UNITY");
+            for (int i = 0; i < this.souls.Count; i++) {
+                if (this.souls[i] == animator.gameObject) {
+                    this.soul = animator.gameObject;
+                }
+            }
         }
     }
 
     private void OnTriggerExit(Collider other) {
-       
+        this.soul = null;
     }
 
 }
