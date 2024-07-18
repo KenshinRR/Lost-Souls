6c82d8b39fc66095ab972f5153bd9639723f1568
railey13 <nikosrailey@gmail.com>
2024-07-18 23:10:24 +0800

    ontrigger detection

--- Assets/Scripts/Lamp.cs
+++ Assets/Scripts/Lamp.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,26 @@ public class Lamp : MonoBehaviour {
     [SerializeField] private GameObject CandleLight;
 
     private Light light;
+    private List<GameObject> souls;
 
     private void Start() {
         this.light = GetComponent<Light>();
-       
+        this.souls = GhostManager.Instance.Possessed;
     }
 
     private void Update() {
-        
+
     }
 
+    private void OnTriggerEnter(Collider other) {
+        Animator animator = other.GetComponentInParent<Animator>();
+        if (animator) {
+            Debug.Log("I HATE UNITY");
+        }
+    }
 
+    private void OnTriggerExit(Collider other) {
+       
+    }
 
 }
