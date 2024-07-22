3d463544876ff726e22727c9866e82887b6fa7dd
railey13 <nikosrailey@gmail.com>
2024-07-22 18:10:27 +0800

    adjusted flame particle and removed jump on player

--- Assets/BrokenVector/LowPolyDungeon/Demo Scenes/PlayerMovement.cs
+++ Assets/BrokenVector/LowPolyDungeon/Demo Scenes/PlayerMovement.cs
@@ -2,42 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    public CharacterController controller;
-    public float baseSpeed = 12f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
-    public float sprintSpeed = 5f;
-
-    float speedBoost = 1f;
-    Vector3 velocity;
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField] private CharacterController PB;
+    [SerializeField] private float playerSpeed = 12.0f;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float sprintSpeed = 5.0f;
 
     private bool onMove = true;
+    private float speedBoost = 1f;
+    private Vector3 velocity;
+   
     private void Move() {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        velocity.y += gravity * Time.deltaTime;
+        this.velocity.y += this.gravity * Time.deltaTime;
 
-        if (Input.GetButton("Fire3"))
-            speedBoost = sprintSpeed;
+        if (Input.GetButton("Left Shift"))
+            this.speedBoost = this.sprintSpeed;
         else
-            speedBoost = 1f;
+            this.speedBoost = 1f;
 
-        if (Input.GetButtonDown("Jump") && controller.isGrounded) {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        if (this.PB.isGrounded && this.velocity.y < 0) {
+            this.velocity.y = -2f;
         }
 
-        if (controller.isGrounded && velocity.y < 0) {
-            velocity.y = -2f;
-        }
+        Vector3 move = this.transform.right * x + this.transform.forward * z;
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        this.PB.Move(move * (this.playerSpeed + this.speedBoost) * Time.deltaTime);
 
-        controller.Move(move * (baseSpeed + speedBoost) * Time.deltaTime);
-
-        controller.Move(velocity * Time.deltaTime);
+        this.PB.Move(this.velocity * Time.deltaTime);
     }
 
     private void OnGameEnd() {
