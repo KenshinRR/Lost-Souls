dd7fb018da16d62a5df26f5bc651f035c9fb7700
KenshinRR <krreblando15@gmail.com>
2024-07-24 13:39:02 +0800

    Fixed problem on reap attempts
    Fixed problem wherein players can still reduce reap attempts during reaping animation by clicking left click

--- Assets/Scripts/Player/ReapHandler.cs
+++ Assets/Scripts/Player/ReapHandler.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class ReapHandler : StateMachineBehaviour
 {
-
+    private bool _isReaping = false;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -15,9 +15,11 @@ public class ReapHandler : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (Input.GetMouseButtonDown(0) &&
-            GhostManager.Instance.ReapAttempts > 0
+            GhostManager.Instance.ReapAttempts > 0 &&
+            !this._isReaping
             )
         {
+            this._isReaping = true;
             GhostManager.Instance.ReapAttempts--;
             animator.SetBool("isReaping", true);
         }
@@ -27,6 +29,7 @@ public class ReapHandler : StateMachineBehaviour
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.SetBool("isReaping", false);
+        this._isReaping = false;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
